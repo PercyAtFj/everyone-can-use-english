ea19471d77205317bc4106653f33b51e9e9b8c2e
lixiancan <lixc.percy@gmail.com>
2024-01-22 22:37:15 +0800

    fix:

--- enjoy/src/main/db/models/conversation.ts
+++ enjoy/src/main/db/models/conversation.ts
@@ -30,6 +30,8 @@ import path from "path";
 import Ffmpeg from "@main/ffmpeg";
 import whisper from "@main/whisper";
 import { hashFile } from "@/utils";
+import { HttpsProxyAgent } from 'https-proxy-agent';
+import * as nf from 'node-fetch';
 
 const logger = log.scope("db/models/conversation");
 @Table({
@@ -145,7 +147,9 @@ export class Conversation extends Model<Conversation> {
         openAIApiKey: key,
         modelName: this.model,
         configuration: {
+          httpAgent: new HttpsProxyAgent("http://127.0.0.1:7892"),
           baseURL: this.configuration.baseUrl,
+          fetch:nf.default,
         },
         temperature: this.configuration.temperature,
         n: this.configuration.numberOfChoices,
